0780855979e13df3bd0e9d0f50386f937dcef07e
kozlik <kozlik>
2004-03-09 16:08:45 +0000

    added domain_from_request_uri to speed dial

--- serweb/html/user_interface/speed_dial.php
+++ serweb/html/user_interface/speed_dial.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Id: speed_dial.php,v 1.2 2004/03/09 12:18:25 kozlik Exp $
+ * $Id: speed_dial.php,v 1.3 2004/03/09 16:08:45 kozlik Exp $
  */
 
 require "prepend.php";
@@ -20,7 +20,7 @@ do{
 
 	if ($dele_sd){
 		$q="delete from ".$config->table_speed_dial." where ".
-			"username='".$auth->auth["uname"]."' and domain='".$config->realm."' and username_from_req_uri=".$dele_sd;
+			"username='".$auth->auth["uname"]."' and domain='".$config->realm."' and username_from_req_uri='".$dele_sd."' and domain_from_req_uri='".$dele_sd_dom."'";
 		$res=mySQL_query($q);
 		if (!$res) {$errors[]="error in SQL query, line: ".__LINE__; break;}
 
@@ -30,8 +30,8 @@ do{
 	}
 
 	if ($edit_sd){
-		$q="select username_from_req_uri, new_request_uri from ".$config->table_speed_dial.
-			" where domain='".$config->realm."' and username='".$auth->auth["uname"]."' and username_from_req_uri=".$edit_sd;
+		$q="select username_from_req_uri, domain_from_req_uri, new_request_uri from ".$config->table_speed_dial.
+			" where domain='".$config->realm."' and username='".$auth->auth["uname"]."' and username_from_req_uri='".$edit_sd."' and domain_from_req_uri='".$edit_sd_dom."'";
 		$res=mySQL_query($q);
 		if (!$res) {$errors[]="error in SQL query, line: ".__LINE__; break;}
 		$row=mysql_fetch_object($res);
@@ -46,6 +46,12 @@ do{
 	                             "valid_e"=>"username from request uri must start by: ".$config->speed_dial_inititation,
 								 "extrahtml"=>"style='width:120px;'"));
 	$f->add_element(array("type"=>"text",
+	                             "name"=>"domain_from_uri",
+								 "size"=>16,
+								 "maxlength"=>128,
+	                             "value"=>$row->domain_from_req_uri?$row->domain_from_req_uri:$config->realm,
+								 "extrahtml"=>"style='width:120px;'"));
+	$f->add_element(array("type"=>"text",
 	                             "name"=>"new_uri",
 								 "size"=>16,
 								 "maxlength"=>128,
@@ -56,6 +62,9 @@ do{
 	$f->add_element(array("type"=>"hidden",
 	                             "name"=>"edit_sd",
 	                             "value"=>$edit_sd?$edit_sd:""));
+	$f->add_element(array("type"=>"hidden",
+	                             "name"=>"edit_sd_dom",
+	                             "value"=>$edit_sd_dom?$edit_sd_dom:""));
 	$f->add_element(array("type"=>"submit",
 	                             "name"=>"okey",
 	                             "src"=>$config->img_src_path."butons/b_save.gif",
@@ -70,10 +79,10 @@ do{
 
 			/* Process data */           // Data ok;
 
-		if ($edit_sd) $q="update ".$config->table_speed_dial." set new_request_uri='$new_uri', username_from_req_uri='$usrnm_from_uri' ".
+		if ($edit_sd) $q="update ".$config->table_speed_dial." set new_request_uri='$new_uri', username_from_req_uri='$usrnm_from_uri', domain_from_req_uri='$domain_from_uri' ".
 			"where username_from_req_uri='$edit_sd' and domain='".$config->realm."' and username='".$auth->auth["uname"]."'";
-		else $q="insert into ".$config->table_speed_dial." (username, domain, username_from_req_uri, new_request_uri) ".
-			"values ('".$auth->auth["uname"]."', '".$config->realm."', '$usrnm_from_uri', '$new_uri')";
+		else $q="insert into ".$config->table_speed_dial." (username, domain, username_from_req_uri, domain_from_req_uri, new_request_uri) ".
+			"values ('".$auth->auth["uname"]."', '".$config->realm."', '$usrnm_from_uri', '$domain_from_uri', '$new_uri')";
 
 		$res=MySQL_Query($q);
 		if (!$res) {$errors[]="error in SQL query, line: ".__LINE__; break;}
@@ -88,10 +97,10 @@ do{
 do{
 	if ($db){
 		// get speed dials
-		if ($edit_sd) $qw=" and username_from_req_uri!='$edit_sd' "; else $qw="";
+		if ($edit_sd) $qw=" and (username_from_req_uri!='$edit_sd' or domain_from_req_uri!='$edit_sd_dom') "; else $qw="";
 
-		$q="select username_from_req_uri, new_request_uri from ".$config->table_speed_dial.
-			" where domain='".$config->realm."' and username='".$auth->auth["uname"]."'".$qw." order by username_from_req_uri";
+		$q="select username_from_req_uri, domain_from_req_uri, new_request_uri from ".$config->table_speed_dial.
+			" where domain='".$config->realm."' and username='".$auth->auth["uname"]."'".$qw." order by domain_from_req_uri, username_from_req_uri";
 		$sd_res=MySQL_Query($q);
 		if (!$sd_res) {$errors[]="error in SQL query, line: ".__LINE__; break;}
 
@@ -143,6 +152,11 @@ if ($okey_x){							//data isn't valid or error in sql
 	<td><?$f->show_element("usrnm_from_uri");?></td>
 	</tr>
 	<tr>
+	<td align="right" class="f12b">domain from request uri:</td>
+	<td width="5">&nbsp;</td>
+	<td><?$f->show_element("domain_from_uri");?></td>
+	</tr>
+	<tr>
 	<td align="right" class="f12b">new request uri:</td>
 	<td width="5">&nbsp;</td>
 	<td><?$f->show_element("new_uri");?></td>
@@ -161,7 +175,7 @@ if ($okey_x){							//data isn't valid or error in sql
 <tr><td>
 	<table border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" align="center">
 	<tr>
-	<td class="titleT">username from request uri</td>
+	<td class="titleT">request uri</td>
 	<td width="2" bgcolor="#C1D773"><img src="<?echo $config->img_src_path;?>title/green_pixel.gif" width="2" height="2"></td>
 	<td class="titleT">new request uri</td>
 	<td width="2" bgcolor="#C1D773"><img src="<?echo $config->img_src_path;?>title/green_pixel.gif" width="2" height="2"></td>
@@ -175,13 +189,13 @@ if ($okey_x){							//data isn't valid or error in sql
 		$odd=$odd?0:1;
 	?>
 	<tr valign="top" <?echo $odd?'bgcolor="#FFFFFF"':'bgcolor="#EAF0F4"';?>>
-	<td align="left" class="f12">&nbsp;<?echo $row->username_from_req_uri;?></td>
+	<td align="left" class="f12">&nbsp;<?echo $row->username_from_req_uri."@".$row->domain_from_req_uri;?></td>
 	<td width="2" bgcolor="#C1D773"><img src="<?echo $config->img_src_path;?>title/green_pixel.gif" width="2" height="2"></td>
 	<td align="left" class="f12"><?echo $row->new_request_uri;?></td>
 	<td width="2" bgcolor="#C1D773"><img src="<?echo $config->img_src_path;?>title/green_pixel.gif" width="2" height="2"></td>
-	<td align="center" class="f12"><a href="<?$sess->purl("speed_dial.php?kvrk=".uniqID("")."&edit_sd=".$row->username_from_req_uri);?>">edit</a></td>
+	<td align="center" class="f12"><a href="<?$sess->purl("speed_dial.php?kvrk=".uniqID("")."&edit_sd=".$row->username_from_req_uri."&edit_sd_dom=".$row->domain_from_req_uri);?>">edit</a></td>
 	<td width="2" bgcolor="#C1D773"><img src="<?echo $config->img_src_path;?>title/green_pixel.gif" width="2" height="2"></td>
-	<td align="center" class="f12"><a href="<?$sess->purl("speed_dial.php?kvrk=".uniqID("")."&dele_sd=".$row->username_from_req_uri);?>">delete</a></td>
+	<td align="center" class="f12"><a href="<?$sess->purl("speed_dial.php?kvrk=".uniqID("")."&dele_sd=".$row->username_from_req_uri."&dele_sd_dom=".$row->domain_from_req_uri);?>">delete</a></td>
 	</tr>
 	<?}?>
 	</table>
