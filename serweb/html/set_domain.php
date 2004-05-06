5ef622d997d3fda5e4cd9d2d1437f52a215dae0b
kozlik <kozlik>
2004-05-06 12:39:58 +0000

    set_domain is now developer friendly

--- serweb/html/set_domain.php
+++ serweb/html/set_domain.php
@@ -1,9 +1,25 @@
 <?
 /*
- * $Id: set_domain.php,v 1.1 2004/03/24 21:39:46 kozlik Exp $
+ * $Id: set_domain.php,v 1.2 2004/05/06 12:39:58 kozlik Exp $
  */ 
 
 /* set domain name */
-$config->domain = ereg_replace( "(www\.|sip\.)?(.*)", "\\2",  $_SERVER['SERVER_NAME']);
+
+/* if set_domain.developer is present, require this instead of setting domain by server name */
+$set_domain_developer = dirname(__FILE__) . "/set_domain.developer.php";
+if (file_exists($set_domain_developer)){
+	require_once ($set_domain_developer);
+}
+else{
+
+/* 	if automatical setting domain by server name form http request doesn't satisfy to you, 
+	comment next line, uncomment the next one and replace 'mydomain.org' string.
+*/
+
+	$config->domain = ereg_replace( "(www\.|sip\.)?(.*)", "\\2",  $_SERVER['SERVER_NAME']);
+//	$config->domain = "mydomain.org";
+
+}
+unset($set_domain_developer);
 
 ?>
