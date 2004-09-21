551bc558c54b89a692a750a129ce4df0bf56c549
kozlik <kozlik>
2004-09-21 13:34:05 +0000

    function confirmDelete() renamed to linkConfirmation()

--- serweb/html/js/functions.js
+++ serweb/html/js/functions.js
@@ -1,7 +1,18 @@
-function confirmDelete(theLink, message){
+/*
+ * $Id: functions.js,v 1.2 2004/09/21 13:34:05 kozlik Exp $
+ */
+
+/* confirm click to <a href=""> */
+
+function linkConfirmation(theLink, message){
     var is_confirmed = confirm(message);
     if (is_confirmed) {
         theLink.href;
     }
     return is_confirmed;
+}
+
+/* deprecated */
+function confirmDelete(theLink, message){
+    return linkConfirmation(theLink, message);
 }
