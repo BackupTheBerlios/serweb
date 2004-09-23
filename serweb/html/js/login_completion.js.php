9713f7df1e584264501328bb2405edf7a692b24d
kozlik <kozlik>
2004-09-23 11:51:55 +0000

    included config.php because it is required by class CReg

--- serweb/html/js/login_completion.js.php
+++ serweb/html/js/login_completion.js.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Id: login_completion.js.php,v 1.2 2004/09/17 19:39:07 kozlik Exp $
+ * $Id: login_completion.js.php,v 1.3 2004/09/23 11:51:55 kozlik Exp $
  */
 
 Header("content-type: text/js");
@@ -11,6 +11,7 @@ $_SERWEB["serwebdir"]  = "../";
 require ($_SERWEB["serwebdir"] . "class_definitions.php");
 require ($_SERWEB["serwebdir"] . "../config/config_paths.php");
 require ($_SERWEB["serwebdir"] . "../config/set_domain.php");
+require ($_SERWEB["serwebdir"] . "../config/config.php");
 require ($_SERWEB["serwebdir"] . "functions.php");
 
 $reg = new Creg;				// create regular expressions class
