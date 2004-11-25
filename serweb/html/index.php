40ab14b610e4214b6962b044072666f303c4c87c
kozlik <kozlik>
2004-11-25 11:23:11 +0000

    index1.php replaced by index.php

--- serweb/html/index.php
+++ serweb/html/index.php
@@ -1,12 +1,12 @@
 <?
 /*
- * $Id: index.php,v 1.1 2004/11/05 19:41:09 kozlik Exp $
+ * $Id: index.php,v 1.2 2004/11/25 11:23:11 kozlik Exp $
  *
  * Redirect to user interface
  *
  */
 
 require "../config/config_paths.php";
-Header("Location: ".$config->user_pages_path."index1.php");
+Header("Location: ".$config->user_pages_path."index.php");
 
 ?>
