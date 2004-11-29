7477ff578ada9c74a786383693c9100295485f6f
kozlik <kozlik>
2004-11-29 09:25:13 +0000

    added class for checking if form was changed

--- serweb/html/js/functions.js
+++ serweb/html/js/functions.js
@@ -1,5 +1,5 @@
 /*
- * $Id: functions.js,v 1.2 2004/09/21 13:34:05 kozlik Exp $
+ * $Id: functions.js,v 1.3 2004/11/29 09:25:13 kozlik Exp $
  */
 
 /* confirm click to <a href=""> */
@@ -16,3 +16,86 @@ function linkConfirmation(theLink, message){
 function confirmDelete(theLink, message){
     return linkConfirmation(theLink, message);
 }
+
+
+/***********************************************************
+ * Functions for display varning message if form is changed
+ ***********************************************************/
+
+	/* main function - add it to onclick event of all <a> tags */
+		
+	function __link_confirmation_if_changed(theLink, message){
+		if (this.changed || this.compareElem()) return linkConfirmation(theLink, message);
+		return true;
+	}
+	
+	/* deprecated */	
+	function __reset_changed(){
+		this.changed=false;
+	}
+	
+	/* deprecated */	
+	function __set_changed(){
+		this.changed=true;
+	}
+	
+	/* save states of form - call it on page load */
+	function __getForm() {
+		//if there are no forms on the page
+		if (!document.forms || !document.forms.length) return false;
+	
+		var f = this.whichForm;
+		var l = f.length;
+		for (var i = 0; i < l; i++) {
+			if (f.elements[i].type == 'text' || f.elements[i].type == 'textarea' || f.elements[i].type == 'select-one') {
+				this.startElem[i] = f.elements[i].value;
+			}
+			if (f.elements[i].type == 'checkbox' || f.elements[i].type == 'radio') {
+				this.startElem[i] = f.elements[i].checked;
+			}
+		}
+	}
+	
+	/* compare form with saved state */
+	function __compareElem() {
+		//if there are no forms on the page
+		if (!document.forms || !document.forms.length) return false;
+		//if startElem wan not filled retrun false
+		if (!this.startElem.length) return false;
+	
+		var f = this.whichForm;
+		var l = f.length;
+		
+		for (var i = 0; i < l; i++) {
+			if ((f.elements[i].type == 'text' || f.elements[i].type == 'textarea' || f.elements[i].type == 'select-one') && this.startElem[i] != f.elements[i].value) {
+				return true;
+			}
+			if ((f.elements[i].type == 'checkbox' || f.elements[i].type == 'radio') && this.startElem[i] != f.elements[i].checked) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	function FormCheck(){
+		var changed;
+		var startElem;		//array to which are stored states of all elements of form
+		var whichForm;
+		
+		this.changed = false;
+		this.startElem = new Array();
+		this.whichForm = document.forms[0];
+		
+		this.reset_changed = __reset_changed;
+		this.set_changed = __set_changed;
+
+		this.link_confirmation_if_changed = __link_confirmation_if_changed;
+		
+		this.getForm = __getForm;
+		this.compareElem = __compareElem;
+		
+		this.getForm();
+	}
+	
+/******************************************************/
