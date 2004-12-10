dc1f3dc227d55f666ada77c48298c74c2159af2a
kozlik <kozlik>
2004-12-10 14:09:38 +0000

    initial version

---
+++ serweb/application_layer/apu_reg_confirmation.php
@@ -0,0 +1,182 @@
+<?
+/*
+ * $Id: apu_reg_confirmation.php,v 1.1 2004/12/10 14:09:38 kozlik Exp $
+ */ 
+
+/* Application unit reg_confirmation */
+
+/*
+   This application unit is used for confirmation of user registration
+   
+   Configuration:
+   --------------
+   
+   'setup_jabber_account'		(bool) default: $config->setup_jabber_account
+     If true, jabber account is setupped for users
+
+	'create_numeric_alias'		(bool) default: $config->create_numeric_alias_to_new_users
+	 If true, create numeric alias for new subscriber
+	
+	'add_to_aliases_table_too'	(bool) default: $config->copy_new_subscribers_to_aliases_table
+	 If true, serweb will add new subscriber also into aliases table instead of into subscriber table only
+	 
+	 
+   'smarty_action'				name of smarty variable - see below
+   'smarty_status'				name of smarty variable - see below
+   
+   Exported smarty variables:
+   --------------------------
+	 
+   opt['smarty_action']			(action)
+	  tells what should smarty display. Values:
+   	  'default' - 
+	  'successfull' - when user registration was confirmed
+
+   opt['smarty_status']
+	  tells aditional info about creating account. Value:
+      'already_done'  -  if account was already created
+	  'jabber_failed' -  if serweb account was created but jabber account not
+	  'ok'
+   
+*/
+
+class apu_reg_confirmation extends apu_base_class{
+	var $smarty_action='default';
+	var $nr;		//confirmation number
+
+	/* return required data layer methods - static class */
+	function get_required_data_layer_methods(){
+		return array('move_user_from_pending_to_subscriber', 'get_new_alias_number', 'add_new_alias',
+									'del_user_from_pending', 'del_user_from_subscriber', 'delete_alias');
+	}
+
+	/* return array of strings - requred javascript files */
+	function get_required_javascript(){
+		return array();
+	}
+	
+	/* constructor */
+	function apu_reg_confirmation(){
+		global $lang_str, $config;
+		parent::apu_base_class();
+
+		/* set default values to $this->opt */		
+		$this->opt['setup_jabber_account'] =		$config->setup_jabber_account;
+
+		/* alias generation */
+		$this->opt['create_numeric_alias'] =		$config->create_numeric_alias_to_new_users;
+		$this->opt['add_to_aliases_table_too'] =	$config->copy_new_subscribers_to_aliases_table;
+		
+		/*** names of variables assigned to smarty ***/
+		/* smarty action */
+		$this->opt['smarty_action'] =		'action';
+
+		$this->opt['smarty_status']	=		'status';
+	}
+
+	function action_confirm_reg(&$errors){
+		global $data;
+
+		// move user from table pending to table subscriber
+		if (false === $user_id=$data->move_user_from_pending_to_subscriber($this->nr, $errors)) return false;
+
+		// if user was already moved
+		if (true === $user_id) {
+			return array("confirmation_ok=1", "conf_already_done=1");
+		} 
+		
+		$remove_from_subscriber=true;
+		$remove_alias=false;
+		do{
+			if ($this->opt['add_to_aliases_table_too']){
+				if (false === $message=$data->add_new_alias($user_id, $user_id->uname, $user_id->domain, $errors)) break;
+			}
+			$remove_alias=true;
+
+			if ($this->opt['create_numeric_alias']){
+				// generate alias number 
+				if (false === $alias=$data->get_new_alias_number($user_id->domain, $errors)) break;
+		
+				// add alias to fifo
+				if (false === $message=$data->add_new_alias($user_id, $alias, $user_id->domain, $errors)) break;
+			}
+			$remove_from_subscriber=false;
+			$remove_alias=false;
+		}while (false);
+			
+		// some error occured during account creating, we should remove user from table subscriber
+		if ($remove_from_subscriber or $remove_alias){
+			if ($remove_from_subscriber) $data->del_user_from_subscriber($this->nr, $errors);
+			if ($remove_alias) $data->delete_alias($user_id, $user_id->uname, $user_id->domain, $errors);
+			return false;
+		}
+			
+		// delete user from table pending			
+		$data->del_user_from_pending($this->nr, $errors);
+	
+		if ($this->opt['setup_jabber_account']) {
+			# Jabber Gateway registration
+			$res = reg_jab($user_id->uname);
+			if($res!=0) {
+				$res=$res+1; 
+				log_errors(PEAR::raise_error("jabber registration failed: <".$user_id->uname."> [".$res."]"), $errors);
+				return array("confirmation_ok=1", "conf_jabber_failed=1");
+			}
+		}
+
+		return array("confirmation_ok=1");
+	}
+	
+	/* this metod is called always at begining */
+	function init(){
+		parent::init();
+	}
+	
+	/* check _get and _post arrays and determine what we will do */
+	function determine_action(){
+		if (isset($_GET['nr'])){
+			$this->nr = $_GET['nr'];
+			$this->action=array('action'=>"confirm_reg",
+			                    'validate_form'=>false,
+								'reload'=>true);
+		}
+		else $this->action=array('action'=>"default",
+			                     'validate_form'=>false,
+								 'reload'=>false);
+	}
+	
+	/* create html form */
+	function create_html_form(&$errors){
+		parent::create_html_form($errors);
+	}
+
+	/* validate html form */
+	function validate_form(&$errors){
+		if (false === parent::validate_form($errors)) return false;
+		return true;
+	}
+	
+	
+	/* add messages to given array */
+	function return_messages(&$msgs){
+		if (isset($_GET['confirmation_ok']) and $_GET['confirmation_ok']) $this->smarty_action = "successfull";
+
+	}
+
+	/* assign variables to smarty */
+	function pass_values_to_html(){
+		global $smarty;
+		$smarty->assign_by_ref($this->opt['smarty_action'], $this->smarty_action);
+		
+		if (isset($_GET['conf_already_done']) and $_GET['conf_already_done']) 
+			$smarty->assign($this->opt['smarty_status'], "already_done");
+		elseif (isset($_GET['conf_jabber_failed']) and $_GET['conf_jabber_failed']) 
+			$smarty->assign($this->opt['smarty_status'], "jabber_failed");
+		else
+			$smarty->assign($this->opt['smarty_status'], "ok");
+	}
+	
+}
+
+
+?>
---
+++ serweb/application_layer/apu_registration.php
@@ -0,0 +1,308 @@
+<?
+
+/*
+ * $Id: apu_registration.php,v 1.1 2004/12/10 14:09:38 kozlik Exp $
+ */ 
+
+/* Application unit registration */
+
+/*
+   This application unit is used for registration new users
+   
+   Configuration:
+   --------------
+
+   'domain'					(string) default: $config->domain
+     domain to which users will be registered
+	 
+   'mail_body'				(string) default: $config->mail_register
+     body of mail which is send after successfull registration
+   
+   'mail_subject'			(string) default: $config->register_subj
+     subject of mail which is send after successfull registration
+   
+   'terms'					(string) default: $config->terms_and_conditions
+     terms and conditions
+   
+   'msg_update'					default: $lang_str['msg_changes_saved_s'] and $lang_str['msg_changes_saved_l']
+     message which should be showed on attributes update - assoc array with keys 'short' and 'long'
+   								
+   'form_name'					(string) default: ''
+     name of html form
+   
+   'form_submit'				(assoc)
+     assotiative array describe submit element of form. For details see description 
+	 of method add_submit in class form_ext
+
+   'smarty_form'				name of smarty variable - see below
+   'smarty_action'				name of smarty variable - see below
+   'smarty_reg_adress'
+   
+   Exported smarty variables:
+   --------------------------
+   opt['smarty_form'] 			(form)			
+     phplib html form
+	 
+   opt['smarty_action']			(action)
+	  tells what should smarty display. Values:
+   	  'default' - 
+	  'finished' - when user submited form and data was succefully stored
+
+   opt['smarty_reg_adress']		(reg_sip_address)
+      contain sip uri of user who registered (avaiable only if smarty_action == finished)
+*/
+
+class apu_registration extends apu_base_class{
+	var $smarty_action='default';
+
+	/* return required data layer methods - static class */
+	function get_required_data_layer_methods(){
+		return array('get_time_zones',  'is_user_exists', 'add_user_to_subscriber');
+	}
+
+	/* return array of strings - requred javascript files */
+	function get_required_javascript(){
+		return array();
+	}
+	
+	/* constructor */
+	function apu_registration(){
+		global $lang_str, $config;
+		parent::apu_base_class();
+
+		/* set default values to $this->opt */		
+
+		$this->opt['domain'] =			$config->domain;
+		$this->opt['mail_body'] =		$config->mail_register;
+		$this->opt['mail_subject'] =	$config->register_subj;
+		$this->opt['terms'] =			$config->terms_and_conditions;
+		
+		/* message on attributes update */
+		$this->opt['msg_update']['short'] =	&$lang_str['msg_changes_saved_s'];
+		$this->opt['msg_update']['long']  =	&$lang_str['msg_changes_saved_l'];
+		
+		/*** names of variables assigned to smarty ***/
+		/* form */
+		$this->opt['smarty_form'] =			'form';
+		/* smarty action */
+		$this->opt['smarty_action'] =		'action';
+		/* name of html form */
+		$this->opt['form_name'] =			'';
+		/* registered sip address */
+		$this->opt['smarty_reg_adress'] = 	'reg_sip_address';
+		
+		$this->opt['form_submit']=array('type' => 'image',
+										'text' => 'save',
+										'src'  => $config->img_src_path."butons/b_save.gif");
+		
+	}
+
+	function action_register(&$errors){
+		global $config, $data, $lang_str;
+		
+		$confirm=md5(uniqid(rand()));
+
+		if (!$data->add_user_to_subscriber($_POST['uname'], $this->opt['domain'], $_POST['passwd'], $_POST['fname'], 
+											$_POST['lname'], $_POST['phone'], $_POST['email'], $_POST['timezone'], 
+											$confirm, $config->data_sql->table_pending, $errors)) return false;
+
+		$sip_address="sip:".$_POST['uname']."@".$this->opt['domain'];
+
+		$mail_body=str_replace("#confirm#", $confirm, $this->opt['mail_body']);
+		$mail_body=str_replace("#sip_address#", $sip_address, $mail_body);
+
+		if (!send_mail($_POST['email'], $this->opt['mail_subject'], $mail_body)){
+			$errors[]=$lang_str['err_sending_mail']; 
+			
+			$this->controler->_form_load_defaults();
+			return false;
+		}
+
+		return array("reg_fi_sip_adr=".RawURLEncode($sip_address),
+		             "reg_finish=".RawURLEncode($this->opt['instance_id']));
+	}
+
+	function action_finish(&$errors){
+		$this->smarty_action="finished";
+	}
+
+	
+	/* this metod is called always at begining */
+	function init(){
+		parent::init();
+
+		$this->reg = new Creg;				// create regular expressions class
+	}
+	
+	/* check _get and _post arrays and determine what we will do */
+	function determine_action(){
+		if ($this->was_form_submited()){	// Is there data to process?
+			$this->action=array('action'=>"register",
+			                    'validate_form'=>true,
+								'reload'=>true);
+		}
+		elseif(isset($_GET['reg_finish']) and $_GET['reg_finish'] == $this->opt['instance_id']){
+			$this->action=array('action'=>"finish",
+			                    'validate_form'=>false,
+								'reload'=>false);
+		}
+		else $this->action=array('action'=>"default",
+			                     'validate_form'=>false,
+								 'reload'=>false);
+	}
+
+	/* create html form */
+	function create_html_form(&$errors){
+		global $lang_str, $data;
+		parent::create_html_form($errors);
+
+		$opt=$data->get_time_zones($errors);
+		$options[]=array("label"=>$lang_str['choose_timezone'],"value"=>"");
+		foreach ($opt as $v) $options[]=array("label"=>$v,"value"=>$v);
+
+		$this->f->add_element(array("type"=>"select",
+									 "name"=>"timezone",
+									 "options"=>$options,
+									 "size"=>1,
+		                             "valid_e"=>$lang_str['fe_not_choosed_timezone'],
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"uname",
+									 "size"=>23,
+									 "maxlength"=>50,
+		                             "value"=>"",
+									 "minlength"=>1,
+									 "length_e"=>$lang_str['fe_not_filled_username'],
+		                             "valid_regex"=>$this->reg->serweb_username,
+		                             "valid_e"=>$lang_str['fe_uname_not_follow_conventions'],
+									 "extrahtml"=>"autocomplete'off' style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"passwd",
+		                             "value"=>"",
+									 "size"=>23,
+									 "maxlength"=>25,
+									 "pass"=>1,
+									 "minlength"=>1,
+									 "length_e"=>$lang_str['fe_not_filled_password'],
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"passwd_r",
+		                             "value"=>"",
+									 "size"=>23,
+									 "maxlength"=>25,
+									 "pass"=>1,
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"fname",
+									 "size"=>23,
+									 "maxlength"=>25,
+		                             "value"=>"",
+									 "minlength"=>1,
+									 "length_e"=>$lang_str['fe_not_filled_your_fname'],
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"lname",
+									 "size"=>23,
+									 "maxlength"=>45,
+		                             "value"=>"",
+									 "minlength"=>1,
+									 "length_e"=>$lang_str['fe_not_filled_your_lname'],
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"email",
+									 "size"=>23,
+									 "maxlength"=>50,
+		                             "value"=>"",
+		                             "valid_regex"=>$this->reg->email,
+		                             "valid_e"=>$lang_str['fe_not_valid_email'],
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"text",
+		                             "name"=>"phone",
+									 "size"=>23,
+									 "maxlength"=>15,
+		                             "value"=>"",
+									 "extrahtml"=>"style='width:250px;'"));
+		$this->f->add_element(array("type"=>"textarea",
+		                             "name"=>"terms",
+		                             "value"=>$this->opt['terms'],
+									 "rows"=>8,
+									 "cols"=>38,
+		                             "wrap"=>"soft",
+									 "extrahtml"=>"style='width:415px;'"));
+		$this->f->add_element(array("type"=>"checkbox",
+		                             "name"=>"accept",
+		                             "value"=>1,
+									 "extrahtml"=>"style=''"));
+		
+	}
+
+	/* validate html form */
+	function validate_form(&$errors){
+		global $lang_str, $data;
+		if (false === parent::validate_form($errors)) return false;
+
+		if ($_POST['passwd'] and ($_POST['passwd'] != $_POST['passwd_r'])){
+			$errors[]=$lang_str['fe_passwords_not_match']; return false;
+		}
+
+		if (!isset($_POST['accept']) or !$_POST['accept']){
+			$errors[]=$lang_str['fe_not_accepted_terms']; return false;
+		}
+
+		if (0 > $user_exists=$data->is_user_exists($_POST['uname'], $this->opt['domain'], $errors)) return false;
+
+		if ($user_exists) {
+			$errors[]=$lang_str['fe_uname_already_choosen_1']." '".$_POST['uname']."' ".$lang_str['fe_uname_already_choosen_2']; 
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	/* add messages to given array */
+	function return_messages(&$msgs){
+		global $_GET;
+		
+/*		if (isset($_GET['m_my_apu_updated']) and $_GET['m_my_apu_updated'] == $this->opt['instance_id']){
+			$msgs[]=&$this->opt['msg_update'];
+			$this->smarty_action="was_updated";
+		}
+ */
+	}
+
+	/* assign variables to smarty */
+	function pass_values_to_html(){
+		global $smarty;
+		$smarty->assign_by_ref($this->opt['smarty_action'], $this->smarty_action);
+		
+		if ($this->smarty_action == "finished")
+			$smarty->assign_by_ref($this->opt['smarty_reg_adress'], $_GET['reg_fi_sip_adr']);
+		
+	}
+	
+	/* return info need to assign html form to smarty */
+	function pass_form_to_html(){
+		global $lang_str;
+
+		$js_after = "if (f.passwd.value!=f.passwd_r.value){
+						alert('".addslashes($lang_str['fe_passwords_not_match'])."');
+						f.passwd.focus();
+						return (false);
+					}
+
+					if (!f.accept.checked){
+						alert('".addslashes($lang_str['fe_not_accepted_terms'])."');
+						f.accept.focus();
+						return (false);
+					}";
+
+
+		return array('smarty_name' => $this->opt['smarty_form'],
+		             'form_name'   => $this->opt['form_name'],
+		             'after'       => $js_after,
+					 'before'      => '');
+	}
+}
+
+?>
