3eb22408d886094adfc6833f022f188321ab7e48
kozlik <kozlik>
2005-03-03 09:53:42 +0000

    - added phpdoc comments - removed obsoleted functions

--- serweb/html/page.php
+++ serweb/html/page.php
@@ -1,6 +1,14 @@
 <?
-/*
- * $Id: page.php,v 1.29 2005/03/02 15:33:55 kozlik Exp $
+/**
+ * Functions for output basic page layout
+ * 
+ * @author    Karel Kozlik
+ * @version   $Id: page.php,v 1.30 2005/03/03 09:53:42 kozlik Exp $
+ * @package   serweb
+ */ 
+
+/**
+ *	Put HTTP headers
  */
 
 function put_headers(){
@@ -8,7 +16,17 @@ function put_headers(){
 	Header("Cache-Control: no-cache");
 	Header("Expires: ".GMDate("D, d M Y H:i:s")." GMT");
 }
-	
+
+/**
+ *	Print begin of html document
+ *	
+ *	Started by &lt;!DOCTYPE....&gt; <br>
+ *	flowed &lt;html&gt;&lt;head&gt;.....
+ *	and ending &lt;/head&gt;
+ *	
+ *	@param string $title Content of html &lt;title&gt; tag
+ */	
+
 function print_html_head($title=""){
 	global $config, $lang_set;	
 	
@@ -24,7 +42,7 @@ function print_html_head($title=""){
 <?if ($title){?><title><?echo $title;?></title><?}?>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=<?echo $lang_set['charset'];?>">
-	<meta name="Author" content="Karel Kozlik <kozlik@kufr.cz>">
+	<meta name="Author" content="Karel Kozlik <karel at iptel dot org>">
 	<meta http-equiv="PRAGMA" content="no-cache"> 
 	<meta http-equiv="Cache-control" content="no-cache">
 	<meta http-equiv="Expires" content="<?echo GMDate("D, d M Y H:i:s")." GMT";?>"> 
@@ -37,6 +55,15 @@ function print_html_head($title=""){
 <?} //end function print_html_head()
 
 
+
+/**
+ *	Print begin of html body
+ * 
+ *  This function should be replaced by smarty template
+ *	
+ *	@param array $parameters associative array containing info about page
+ *  @deprecated
+ */	
 
 function print_html_body_begin(&$parameters){
 	global $config, $sess, $auth, $errors, $message;
@@ -59,6 +86,14 @@ function print_html_body_begin(&$parameters){
 } //end function print_html_body_begin
 
 	
+/**
+ *	Print end of html body
+ * 
+ *  This function should be replaced by smarty template
+ *	
+ *  @deprecated
+ */	
+
 function print_html_body_end(){
 	global $config, $_page_tab;	?>
 	</div>
@@ -67,64 +102,5 @@ function print_html_body_end(){
 	virtual(multidomain_get_file($config->html_epilog));
 }
 
-function print_tabs($tabs, $path="", $selected=null){
-	global $config, $sess, $PATH_TRANSLATED;
-
-	if (!$selected){
-		$selected=basename($PATH_TRANSLATED);
-	}
-?>
-	<div id="swTabs">
-		<ul>
-<? 
-
-foreach($tabs as $i => $value){
-	if ($value->enabled){
-		if ($value->page==$selected){?>
-		<li id="swActiveTab"><div class="swTabsL"></div><strong><?echo $value->name;?></strong><div class="swTabsR"></div></li>
-<?		}
-		else{?>
-			<li><div class="swTabsL"></div><a href="<?$sess->purl($path.$value->page."?kvrk=".uniqID(""));?>" class="tabl"><?echo $value->name;?></a><div class="swTabsR"></div></li>
-<?		}//if ($value->page==$selected)
-	}// if ($value->enabled)
-} //foreach		?>
-		</ul>
-	</div>
-
-<?
-} //end function print_tabs
-
-
-	function print_resell_html_body_begin($tab_selected=null){
-		global $config, $_page_tab, $sess, $auth;
-
-		virtual($config->prolog);
-		echo $config->realm." Reseller Management";
-		virtual($config->separator);
-
-		?>
-			<div class="f12">
-				<table width="200" align="right">
-				<td align="right">&nbsp;<a href=logout.php>Logout</a></td>
-				<td align="right">&nbsp;<a href=/phpBB/>FAQ</a></td>
-				</tr>
-				</table>
-			</div>
-			<br clear="all">
-		<?
-		
-		if ($auth->auth["uname"]==$config->ppaid->operator_id)
-			$tabs=$config->ppaid->op_tabs;
-		else
-			$tabs=$config->ppaid->r_tabs;
-		
-		print_tabs_new($tabs, $config->ppaid->resell_pages_path, $tab_selected); $_page_tab=1;?>	
-
- 		<table bgcolor="#B1C9DC" width="100%" border="0" cellspacing="0" cellpadding="1">
-		<tr><td>
-			<table bgcolor="#FFFFFF" width="100%" border="0" cellspacing="0" cellpadding="20">
-			<tr valign="top"><td>
-			
-<?	} //print_resell_html_body_begin
 
 ?>
