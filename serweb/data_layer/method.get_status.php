53aa81d42eed0bf632ef15aebff0ae6640c45d0d
kozlik <kozlik>
2005-05-02 14:38:29 +0000

    xxl support

--- serweb/data_layer/method.get_status.php
+++ serweb/data_layer/method.get_status.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Id: method.get_status.php,v 1.2 2005/04/21 15:09:45 kozlik Exp $
+ * $Id: method.get_status.php,v 1.3 2005/05/02 14:38:29 kozlik Exp $
  */
 
 class CData_Layer_get_status {
@@ -12,17 +12,34 @@ class CData_Layer_get_status {
 	 */
 
 	function get_status($sip_uri, &$errors){
-		global $config, $lang_str;
+		global $config, $lang_str, $data;
 
-		$reg=new Creg;
-		if (!eregi("^sip:([^@]+@)?".$reg->host, $sip_uri, $regs)) return "<div class=\"statusunknown\">".$lang_str['status_nonlocal']."</div>";
+		/* create connection to proxy where are stored data of user */
+		if ($config->enable_XXL and $this->name != "get_status_tmp"){
+
+			$tmp_data = CData_Layer::singleton("get_status_tmp", $errors);
+			$tmp_data->set_xxl_user_id($sip_uri);
+			//$tmp_data->expect_user_id_may_not_exists(); //do not need this?
+
+			return $tmp_data->get_status($sip_uri, $errors);
+		}
+
+
+		$reg = Creg::singleton();
+		if (!eregi("^sip:([^@]+@)?".$reg->host, $sip_uri, $regs)) 
+			return "<div class=\"statusunknown\">".$lang_str['status_nonlocal']."</div>";
 
 		$user=substr($regs[1],0,-1);
 		$domain=$regs[2];
 
-		$local = $this->domain_exists($domain, $errors);
-		if ($local < 0) return "<div class=\"statusunknown\">".$lang_str['status_unknown']."</div>";
-		if (! $local) return "<div class=\"statusunknown\">".$lang_str['status_nonlocal']."</div>";
+		if ($config->multidomain){
+			$local = $data->domain_exists($domain, $errors);
+			if ($local < 0) return "<div class=\"statusunknown\">".$lang_str['status_unknown']."</div>";
+			if (! $local) return "<div class=\"statusunknown\">".$lang_str['status_nonlocal']."</div>";
+		}
+		else{
+			if ($domain != $config->domain) return "<div class=\"statusunknown\">".$lang_str['status_nonlocal']."</div>";
+		}
 
 
 		//check if user exists
@@ -39,15 +56,37 @@ class CData_Layer_get_status {
 		
 		//check usrloc if user is online
 		
-		$fifo_cmd=":ul_show_contact:".$config->reply_fifo_filename."\n".
-		$config->ul_table."\n".		//table
-		$user."@".$domain."\n\n";	//username
+		if ($config->use_rpc){
+			if (!$this->connect_to_xml_rpc(null, $errors)) 
+				return "<div class=\"statusunknown\">".$lang_str['status_unknown']."</div>";
+			
+			$params = array(new XML_RPC_Value($config->ul_table, 'string'),
+			                new XML_RPC_Value($user."@".$domain, 'string'));
+			$msg = new XML_RPC_Message_patched('ul_show_contact', $params);
+			$res = $this->rpc->send($msg);
+	
+			if ($this->rpc_is_error($res)){
+				if ($res->getCode() == "404"){
+					return "<div class=\"statusoffline\">".$lang_str['status_offline']."</div>";
+				}
 
-		$out=write2fifo($fifo_cmd, $errors, $status);
-		if ($errors) return;
+				log_errors($res, $errors); 
+				return "<div class=\"statusunknown\">".$lang_str['status_unknown']."</div>";
+			}
+			return "<div class=\"statusonline\">".$lang_str['status_online']."</div>";
+		}
+		else{
 
-		if (substr($status,0,3)=="200") return "<div class=\"statusonline\">".$lang_str['status_online']."</div>";
-		else return "<div class=\"statusoffline\">".$lang_str['status_offline']."</div>";
+			$fifo_cmd=":ul_show_contact:".$config->reply_fifo_filename."\n".
+			$config->ul_table."\n".		//table
+			$user."@".$domain."\n\n";	//username
+
+			$out=write2fifo($fifo_cmd, $errors, $status);
+			if ($errors) return;
+
+			if (substr($status,0,3)=="200") return "<div class=\"statusonline\">".$lang_str['status_online']."</div>";
+			else return "<div class=\"statusoffline\">".$lang_str['status_offline']."</div>";
+		}
 	}
 	
 }
