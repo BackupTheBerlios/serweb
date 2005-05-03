3fa5019bd2440b657754e33b171dc8296265ae9b
kozlik <kozlik>
2005-05-03 09:25:14 +0000

    rewrited some methods needed for xxl

--- serweb/html/data_layer.php
+++ serweb/html/data_layer.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Id: data_layer.php,v 1.6 2005/04/21 15:09:46 kozlik Exp $
+ * $Id: data_layer.php,v 1.7 2005/05/03 09:25:14 kozlik Exp $
  */
 
 // variable $_data_layer_required_methods should be defined at beginning of each php script
@@ -19,10 +19,9 @@ class CData_Layer{
 
 	var $xxl_user_id = null;
 
-	var $xxl_proxy = null;
-	var $xxl_db_dsn = null;
-	
 	var $name = "auth_user";			//name of instance
+
+	var $expect_user_id_not_ex = false;
 
 	/*
 	 *   Constructor
@@ -40,6 +39,7 @@ class CData_Layer{
      *	@return object CData_Layer		instance of CData_Layer class
      *	@static
      *	@access public 
+     *	@deprec
 	 */
 
 	function &create(&$errors){
@@ -64,6 +64,7 @@ class CData_Layer{
 	 */
 	function &singleton($instance_name, &$errors){
 		static $instances = array();
+		global $sess_data_conn;
 
 		if (!$instance_name) $instance_name = "auth_user";
 
@@ -71,6 +72,11 @@ class CData_Layer{
 			$instances[$instance_name] = &CData_Layer::create($errors);
 			$instances[$instance_name]->name = $instance_name;
 		}
+
+		if (!isset($sess_data_conn[$instance_name])) 
+			$sess_data_conn[$instance_name] = array('proxy' => null,
+		        	                                'db_dsn' => null,
+												    'expire' => 0);
 		
 		return $instances[$instance_name];
 	}
@@ -105,167 +111,134 @@ class CData_Layer{
 			$_data_layer_required_methods = array();
 			
 		$_data_layer_required_methods = array_merge($config->data_layer_always_required_functions, $_data_layer_required_methods);
+
+
+		$loaded_modules = getLoadedModules();
 		
 		reset($_data_layer_required_methods);
 		while (list(, $item) = each($_data_layer_required_methods)) {
 		
 			if (false ===  array_search($item, $this->_data_layer_loaded_methods)){ //if required method isn't loaded yet, load it
-			
+
+				$file_found = false;			
 				//require class with method definition
-				if (file_exists($_SERWEB["serwebdir"] . "../data_layer/customized/method.".$item.".php")) 
+				if (file_exists($_SERWEB["serwebdir"] . "../data_layer/customized/method.".$item.".php")){ 
 					//if exists customized version of method, require it
 					require_once ($_SERWEB["serwebdir"] . "../data_layer/customized/method.".$item.".php");
-				else
+
+					$file_found = true;
+				}
+				
+				if (!$file_found){
+					//try found file in modules
+					foreach($loaded_modules as $module){
+						if (file_exists($_SERWEB["serwebdir"] . "../modules/".$module."/method.".$item.".php")){ 
+							require_once ($_SERWEB["serwebdir"] . "../modules/".$module."/method.".$item.".php");
+							$file_found = true;
+							break;
+						}
+					}
+				}
+
+				if (!$file_found){
 					//otherwise require default version
 					require_once ($_SERWEB["serwebdir"] . "../data_layer/method.".$item.".php");	
+				}
 					
 				//agregate methods of required class to this object
-				aggregate_methods($this, "CData_Layer_".$item);								
+				my_aggregate_methods($this, "CData_Layer_".$item);								
 				
 				//add method to $_data_layer_loaded_methods array
 				$this->_data_layer_loaded_methods[] = $item;										
 				
 				//add methods required by currently loaded method to $_data_layer_required_methods array
-				$class_vars = get_class_vars("CData_Layer_".$item);							
-				if (isset($class_vars['required_methods']) and is_array($class_vars['required_methods'])){
-					$_data_layer_required_methods = array_merge($_data_layer_required_methods, $class_vars['required_methods']);
+				$class_methods = get_class_methods("CData_Layer_".$item);							
+
+				if (in_array('_get_required_methods', $class_methods)){
+					$_data_layer_required_methods = 
+						array_merge($_data_layer_required_methods, 
+									call_user_func(array("CData_Layer_".$item, '_get_required_methods')));
 				}
+				else{
+					$class_vars = get_class_vars("CData_Layer_".$item);							
+					if (isset($class_vars['required_methods']) and is_array($class_vars['required_methods'])){
+						$_data_layer_required_methods = 
+							array_merge($_data_layer_required_methods, 
+							            $class_vars['required_methods']);
+					}
+				}
+
 			}
 		}
 	}
+
 
 	/**
 	 *	Ask SER cluster on proxy which is assignet to user
 	 *
 	 *	@param String $user_id		sip uri of iser or urn with uuid of user (format of urn is: 'urn:uuid:foobar')
+	 *	@return String 				sip uri of proxy to which is user assignet or FALSE on error
 	 *
-	 *	@return String 	sip uri of proxy to which is user assignet or FALSE on error
-	 *
-	 *	@todo test if this function work correctly and remove testing return statement
 	 */	
-	function lookup_proxy($user_id){
-		global $config;
-
-		$cli = new XML_RPC_Client('/'.$user_id, $config->ser_rpc['host'], $config->ser_rpc['port']);
+	function get_proxy($user_id, &$errors){
+		if (!$this->connect_to_xml_rpc(array('uri' => $user_id), $errors)) return false;
 		
 		$params = array(new XML_RPC_Value($user_id, 'string'));
 		$msg = new XML_RPC_Message_patched('get_proxy', $params);
-		$res = $cli->send($msg);
+		$res = $this->rpc->send($msg);
 
-		if ($this->rpc_is_error($res, $cli)){
-			log_errors($res, $errors); return false;
+		if ($this->rpc_is_error($res)){
+			if (! $this->expect_user_id_not_ex){
+				log_errors($res, $errors); 
+			}
+			return false;
 		}
 
 	    $val = $res->value();
-		$proxy_uri = trim($val->scalarval());
+		
+		$proxies = explode("\n", $val->scalarval()); //when SER return more porxies for user, use only the first
+		$proxy_uri = trim($proxies[0]);
 
 		if (!$proxy_uri){
-			sw_log("Lookup_proxy: invalid answer from get_proxy", PEAR_LOG_ERR);
+			sw_log("Get_proxy: invalid answer", PEAR_LOG_ERR);
 			return false;
 		}
 
-		sw_log("Lookup_proxy: ".$proxy_uri." assigned to user ".$user_id, PEAR_LOG_DEBUG);
-			
+		sw_log("Get_proxy: ".$proxy_uri." assigned to user ".$user_id, PEAR_LOG_DEBUG);
 
-
-		$cli = new XML_RPC_Client('/'.$proxy_uri, $config->ser_rpc['host'], $config->ser_rpc['port']);
-
-		$msg = new XML_RPC_Message_patched('get_db_uri');
-		$res = $cli->send($msg);
-
-		if ($this->rpc_is_error($res, $cli)){
-			log_errors($res, $errors); return false;		}
-
-	    $val = $res->value();
-		$db_uri = trim($val->scalarval());
-
-		if (!$db_uri){
-			sw_log("Lookup_proxy: invalid answer from get_db_uri", PEAR_LOG_ERR);
-			return false;
-		}
-
-		sw_log("Lookup_proxy: ".$db_uri." assigned to proxy ".$proxy_uri, PEAR_LOG_DEBUG);
-
-			
-		return array('proxy' => $proxy_uri,
-           			 'db' => $db_uri);
+		return $proxy_uri;
 	}
 
-	/**
-	 *	Set session variable $sess_xxl_proxy 
-	 *	by uri returned from function lookup_proxy()
-	 *
-	 *	@param string $sip_uri
-	 *	@return bool	TRUE on success, FALSE on bad format of $sip_uri
-	 *	@access private
-	 */
-	function set_sess_xxl_proxy($proxy){
-		global $config, $sess_xxl_proxy;
-		
-		/* get instance of Creg class */
-		$reg = Creg::singleton();
-		
-		$sess_xxl_proxy[$this->name] = $proxy;
-		
-		/* parse parameters from sip uri of proxy */
-		$params = $reg->get_parameters($proxy['proxy']);
-		
-		if (isset($params['expire'])) {
-			/* if sip uri contain parameter 'expire', use it for calculate new expiration time */
-			$sess_xxl_proxy[$this->name]['expire'] = time() + $params['expire'];
-			sw_log("Expiration time of proxy assigment is set by uri parameter to ".$params['expire']." seconds", PEAR_LOG_DEBUG);
-		}
-		else {
-			/* else use config variable for calculation expiration time */
-			$sess_xxl_proxy[$this->name]['expire'] = time() + $config->XXL_proxy_asigment_lifetime;
-			sw_log("Expiration time of proxy assigment is set by config variable to ".$config->XXL_proxy_asigment_lifetime." seconds", PEAR_LOG_DEBUG);
-		}
-		
-		return true;
+	function expect_user_id_may_not_exists(){
+		$this->expect_user_id_not_ex = true;
 	}
 
-	/**
-	 *	Set user id for proxy lookup in XXL version. 
-	 *	Calling this function force call proxy_lookup function even though
-	 *	proxy url is known
-	 *
-	 *	@param String $user_id		sip uri of iser or urn with uuid of user (format of urn is: 'urn:uuid:foobar')
-	 */
-	function set_xxl_user_id($user_id){
-		$this->xxl_user_id = $user_id;
-	}
 
 	/**
-	 *	Return hostname of DB server
+	 *	Return home proxy of user
 	 *	
-	 *	@return string 		DB host or FALSE on error
+	 *	@return String 		sip uri of proxy or FALSE on error
 	 *	@access private
 	 */
-	function find_xxl_proxy(){
-		global $sess, $sess_xxl_proxy, $serweb_auth, $config;
-		
-		if (!$sess->is_registered('sess_xxl_proxy')) $sess->register('sess_xxl_proxy');
-		if (!isset($sess_xxl_proxy[$this->name])) 
-			$sess_xxl_proxy[$this->name] = array('proxy' => null,
-		        	                             'db' => null,
-												 'expire' => 0);
+	function get_home_proxy(&$errors){
+		global $sess, $sess_data_conn, $serweb_auth, $config;
 
-
-		// if $this->xxl_user_id is set, force calling lookup_proxy
+		// if $this->xxl_user_id is set, force calling get_proxy
 		if ($this->xxl_user_id){
-			if (false === $proxy = $this->lookup_proxy($this->xxl_user_id)){
+			if (false === $proxy = $this->get_proxy($this->xxl_user_id, $errors)){
 				return false;
 			}
 			
-			if (!$this->set_sess_xxl_proxy($proxy)) return false;
-			return $sess_xxl_proxy[$this->name];
+			if (!$this->set_sess_data_proxy($proxy)) return false;
+			return $sess_data_conn[$this->name]['proxy'];
 		}
 
 		// if db host is in session and not expired yet, return it
-		if ($sess_xxl_proxy[$this->name] and 
-				(!isset($sess_xxl_proxy[$this->name]['expire']) or 
-				 $sess_xxl_proxy[$this->name]['expire'] > time())){
-			return $sess_xxl_proxy[$this->name];
+		if ($sess_data_conn[$this->name] and 
+				(!isset($sess_data_conn[$this->name]['expire']) or 
+				 $sess_data_conn[$this->name]['expire'] > time())){
+
+			return $sess_data_conn[$this->name]['proxy'];
 		}
 
 		// if $serweb_auth exists use it for obtain uri of sip proxy
@@ -277,35 +250,16 @@ class CData_Layer{
 				$user_id = 'sip:'.$serweb_auth->uname."@".$serweb_auth->domain;
 			}
 
-			if (false === $proxy = $this->lookup_proxy($user_id)){
+			if (false === $proxy = $this->get_proxy($user_id, $errors)){
 				return false;
 			}
 			
-			if (!$this->set_sess_xxl_proxy($proxy)) return false;
-			return $sess_xxl_proxy[$this->name];
+			if (!$this->set_sess_data_proxy($proxy)) return false;
+			return $sess_data_conn[$this->name]['proxy'];
 		}
 
-		sw_log("Can't find id of user to give it to function lookup_proxy", PEAR_LOG_ERR);
+		sw_log("get_home_proxy: Can't find id of user to fing his home proxy", PEAR_LOG_ERR);
 		return false;
-	}
-
-	/**
-	 *	Set internal variables $xxl_proxy and $xxl_db_dsn
-	 *
-	 *	@return bool FALSE on error
-	 *	@access private
-	 */
-	 
-	function set_xxl_connection(&$errors){
-		//get url of DB host which should be used 
-		if (false === $proxy = $this->find_xxl_proxy()){
-			log_errors(PEAR::raiseError("Can't get URL of DB server"), $errors); return false; 
-		}
-			
-		$this->xxl_proxy = $proxy['proxy'];
-		$this->xxl_db_dsn = $proxy['db'];
-		
-		return true;
 	}
 
 
@@ -314,43 +268,68 @@ class CData_Layer{
 	 *	@access public
 	 */
 	 
-	function set_xxl_proxy(&$proxy){
+	function set_home_proxy($proxy){
+		global $sess_data_conn;
 
-		if (!isset($proxy['db'])){
-			if (false === $db_uri = $this->get_db_uri($proxy['proxy'])) return false;
-			$proxy['db'] = $db_uri;
+		/* if proxy is changed, unset db_dsn */
+		if ($sess_data_conn[$this->name]['proxy'] != $proxy){
+			$sess_data_conn[$this->name]['db_dsn'] = null;	
 		}
 
-		$this->xxl_proxy = $proxy['proxy'];
-		$this->xxl_db_dsn = $proxy['db'];
-		
-		return true;
+		$this->set_sess_data_proxy($proxy);
 	}
 
 
 	/**
-	 *	Get proxy to which is this instance connected
-	 *	@return string	sip uri of proxy or FALSE on error
-	 *	@access public
+	 *	Set proxy in session variable $sess_data_conn 
+	 *	by uri returned from function lookup_proxy()
+	 *
+	 *	@param string $proxy	sip uri of proxy
+	 *	@return bool			TRUE on success, FALSE on bad format of $sip_uri
+	 *	@access private
 	 */
+	function set_sess_data_proxy($proxy){
+		global $config, $sess, $sess_data_conn;
 
-	function get_xxl_proxy(&$errors){
-		global $config;
+		if (!$sess->is_registered('sess_data_conn')) $sess->register('sess_data_conn');
 		
-		if ($config->enable_XXL){
-			/* if not set proxy to connect, get it */
-			if (! $this->xxl_proxy){
-				if(false === $this->set_xxl_connection($errors)) {
-					return false;
-				}
-			}
-			
-			return $this->xxl_proxy;
+		/* get instance of Creg class */
+		$reg = Creg::singleton();
+		
+		$sess_data_conn[$this->name]['proxy'] = $proxy;
+		
+		/* parse parameters from sip uri of proxy */
+		$params = $reg->get_parameters($proxy);
+		
+		if (isset($params['expire'])) {
+			/* if sip uri contain parameter 'expire', use it for calculate new expiration time */
+			$sess_data_conn[$this->name]['expire'] = time() + $params['expire'];
+			sw_log("Expiration time of proxy assigment is set by uri parameter to ".$params['expire']." seconds", PEAR_LOG_DEBUG);
 		}
-		else
-			return "sip:".$config->ser_rpc['host'];
+		else {
+			/* else use config variable for calculation expiration time */
+			$sess_data_conn[$this->name]['expire'] = time() + $config->XXL_proxy_asigment_lifetime;
+			sw_log("Expiration time of proxy assigment is set by config variable to ".$config->XXL_proxy_asigment_lifetime." seconds", PEAR_LOG_DEBUG);
+		}
+
+		return true;
 	}
 
+	/**
+	 *	Set user id for get home proxy in XXL version. 
+	 *	Calling this function force call proxy_lookup function even though
+	 *	proxy url is known
+	 *
+	 *	@param String $user_id		sip uri of iser or urn with uuid of user (format of urn is: 'urn:uuid:foobar')
+	 */
+	function set_xxl_user_id($user_id){
+		global $sess_data_conn;
+		$this->xxl_user_id = $user_id;
+		
+		/* unset conection info */
+		$sess_data_conn[$this->name]['proxy'] = null;
+		$sess_data_conn[$this->name]['db_dsn'] = null;
+	}
 
 	/**
 	 *	Get dsn of DB used by given sip proxy
@@ -358,15 +337,16 @@ class CData_Layer{
 	 *	@param string $proxy_uri	uri of sip proxy
 	 *	@return string				dsn of DB
 	 */
-	function get_db_uri($proxy_uri){
-		if (!$this->connect_to_xml_rpc($proxy_uri, $errors)) return false;
+	function get_db_uri($proxy_uri, &$errors){
+		if (!$this->connect_to_xml_rpc(array("uri" => $proxy_uri), $errors)) 
+			return false;
 
 		$msg = new XML_RPC_Message_patched('get_db_uri');
-		
 		$res = $this->rpc->send($msg);
 
 		if ($this->rpc_is_error($res)){
-			log_errors($res, $errors); return false;		}
+			log_errors($res, $errors); return false;		
+		}
 		
 	    $val = $res->value();
 		$val = trim($val->scalarval());
@@ -376,6 +356,7 @@ class CData_Layer{
 		return $val;
 	}
 
+
 	/**
 	 *	Check if there was error during xml_rpc request
 	 *
@@ -383,12 +364,10 @@ class CData_Layer{
 	 *	@param object $client	instance of XML_RPC_Client, $this->rpc is used by default
 	 *	@return bool			true on error, false on everything is OK
 	 */
-	function rpc_is_error(&$resp, $client=null){
+	function rpc_is_error(&$resp){
 		if (!$resp) {
-			if (!$client) $client = &$this->rpc;
-		
 			$resp = PEAR::raiseError("xml_rpc communication error",
-									 null, null, null, $client->errstr);
+									 null, null, null, $this->rpc->errstr);
 			return true;
 		}
 		
@@ -405,7 +384,7 @@ class CData_Layer{
 				$lines = explode("\n", $val);
 				
 				$resp = PEAR::raiseError("xml_rpc request error",
-										 null, null, null, $lines[0]);
+										 substr($val, 0, 3), null, null, $lines[0]);
 				return true;
 			}
 		}
@@ -418,37 +397,50 @@ class CData_Layer{
 	 */
 
 	function connect_to_db(&$errors){
-		global $config;
+		global $config, $sess_data_conn;
 
 		if ($this->db) return $this->db;
 
-		$cfg=&$config->data_sql;
 
-		if ($config->enable_XXL){
-			//use only first definition of DB host
-			$serv=0;
-			$num=1;
+		if ($config->use_rpc){
 
 			//if not set DSN of DB to connect, get it
-			if (! $this->xxl_db_dsn){
-				if(false === $this->set_xxl_connection($errors)) return false;
+			if (! $sess_data_conn[$this->name]['db_dsn']){
+				if ($config->enable_XXL){
+					if(false === $this->get_home_proxy($errors)) return false;
+	
+					if(false === $sess_data_conn[$this->name]['db_dsn'] = 
+								$this->get_db_uri($sess_data_conn[$this->name]['proxy'], $errors)) 
+						return false;
+				}
+				else{
+					if(false === $sess_data_conn[$this->name]['db_dsn'] = 
+								$this->get_db_uri("sip:".$config->ser_rpc['host'], $errors)) 
+						return false;
+				}
 			}
-			
+
+			$dsn = $sess_data_conn[$this->name]['db_dsn'];
+			$db = DB::connect($dsn, true);
+
+
+			if (DB::isError($db)) {	
+				log_errors($db, $errors); return false; 
+			}
+
 		}
 		else{
-			$num=count($cfg->host); //get number of SQL servers that we can use
 
+			$cfg=&$config->data_sql;
+	
+			$num=count($cfg->host); //get number of SQL servers that we can use
+	
 			if ($num>1)	$serv=mt_rand(0, $num-1);
 			else $serv=0;
-		}
-		
-		$tries=0;
-		do{		
-			$cont=0;
-			if ($config->enable_XXL){
-				$dsn = $this->xxl_db_dsn;
-			}
-			else{
+			
+			$tries=0;
+			do{		
+				$cont=0;
 				$dsn = 	$cfg->type."://".
 						$cfg->host[$serv]['user'].":".
 						$cfg->host[$serv]['pass']."@".
@@ -457,61 +449,57 @@ class CData_Layer{
 								"":
 								":".$cfg->host[$serv]['port'])."/".
 						$cfg->host[$serv]['name'];
-			}
-	
-			$db = DB::connect($dsn, true);
-	
-			if (DB::isError($db)) {	
-				//if connect failed and multiple servers is defined
-				if (($db->getCode() == DB_ERROR_CONNECT_FAILED) and ($num>1)){ 
-					//try another server
-					$tries++;
-					$serv++; $serv %= $num;
-					if ($tries<$num) {
-						$cont=1; continue;
+		
+				$db = DB::connect($dsn, true);
+		
+				if (DB::isError($db)) {	
+					//if connect failed and multiple servers is defined
+					if (($db->getCode() == DB_ERROR_CONNECT_FAILED) and ($num>1)){ 
+						//try another server
+						$tries++;
+						$serv++; $serv %= $num;
+						if ($tries<$num) {
+							$cont=1; continue;
+						}
 					}
+					log_errors($db, $errors); return false; 
 				}
-				log_errors($db, $errors); return false; 
-			}
-		}while($cont);
+			}while($cont);
+		}
 
 		$this->db=$db;
 		return $db;
 	}
 
+
 	/**
 	 *	create instance of xml rpc client 
 	 */
-	function connect_to_xml_rpc($user_uri, &$errors){
-		global $config;
+	function connect_to_xml_rpc($opt, &$errors){
+		global $config, $sess_data_conn;
 
-		if (!$user_uri){
-			$user_uri = "sip:".$config->ser_rpc['host'];
+		if (isset($opt['cluster'])) {
+			$proxy = "sip:".$config->ser_rpc['host'];
 		}
-
-
-		if ($this->rpc and $this->rpc->path == '/'.$user_uri) return $this->rpc;
-
-
-		if ($config->enable_XXL){
-
-			/* if not set proxy to connect, get it */
-			if (! $this->xxl_proxy){
-				if(false === $this->set_xxl_connection($errors)) return false;
+		else if (isset($opt['uri'])){
+			$proxy = $opt['uri'];
+		}
+		else {
+			if (! $sess_data_conn[$this->name]['proxy']){
+				if ($config->enable_XXL){
+					if (false === $this->get_home_proxy($errors)) return false;
+				}
+				else {
+					$this->set_home_proxy("sip:".$config->ser_rpc['host']);
+				}
 			}
-
-			/* get instance of Creg class */
-			$reg = Creg::singleton();
 			
-			$proxy_host = $reg->get_domainname($this->xxl_proxy);
-
-			$rpc = new XML_RPC_Client('/'.$user_uri, $proxy_host, $config->ser_rpc['port']);
-			
+			$proxy = $sess_data_conn[$this->name]['proxy'];
 		}
-		else{
-			$rpc = new XML_RPC_Client('/'.$user_uri, $config->ser_rpc['host'], $config->ser_rpc['port']);
-			
-		}
+
+		if ($this->rpc and $this->rpc->path == '/'.$proxy) return $this->rpc;
+
+		$rpc = new XML_RPC_Client('/'.$proxy, $config->ser_rpc['host'], $config->ser_rpc['port']);
 
 		$this->rpc=&$rpc;
 		return $rpc;
