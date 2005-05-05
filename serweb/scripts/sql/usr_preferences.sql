32b4bf8f2a5153f02bb9332874eb396486bf96e4
kozlik <kozlik>
2005-05-05 12:05:57 +0000

    initial version

---
+++ serweb/scripts/sql/usr_preferences.sql
@@ -0,0 +1,8 @@
+#
+# $Id: usr_preferences.sql,v 1.1 2005/05/05 12:05:57 kozlik Exp $
+#
+# Dumping data for table 'usr_preferences_types'
+#
+
+INSERT INTO usr_preferences_types VALUES("fw_voicemail", "boolean", "1", NULL, "0");
+INSERT INTO usr_preferences_types VALUES("sw_user_status_visible", "boolean", "1", NULL, "1");
