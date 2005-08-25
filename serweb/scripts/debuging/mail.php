8acad166483e286d4d669f21ca6cf0980789a795
kozlik <kozlik>
2005-08-25 07:50:29 +0000

    removed my email address

--- serweb/scripts/debuging/mail.php
+++ serweb/scripts/debuging/mail.php
@@ -1,8 +1,7 @@
 <?php
-// $Id: mail.php,v 1.2 2005/07/21 16:25:17 kozlik Exp $
+// $Id: mail.php,v 1.3 2005/08/25 07:50:29 kozlik Exp $
 
 	$to      = "root@localhost";
-	$to      = "karel@iptel.org";
 	$subject = "php	testing mail";
 	$body    = "GREAT! \n it working\n";
 	
