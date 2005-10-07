70ef8744e514f595e6fe8dcf9621883d21178160
kozlik <kozlik>
2005-10-07 14:17:14 +0000

    limit admin access to owned domain

--- serweb/data_layer/method.check_admin_perms_to_domain.php
+++ serweb/data_layer/method.check_admin_perms_to_domain.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Id: method.check_admin_perms_to_domain.php,v 1.1 2005/09/26 10:56:54 kozlik Exp $
+ * $Id: method.check_admin_perms_to_domain.php,v 1.2 2005/10/07 14:17:14 kozlik Exp $
  */
 
 class CData_Layer_check_admin_perms_to_domain {
@@ -13,15 +13,14 @@ class CData_Layer_check_admin_perms_to_domain {
 	 *	 none
 	 *
 	 *	@param object $admin		admin - instance of class Cserweb_auth
-	 *	@param object $auth			instance of class Auth
 	 *	@param string $domain_id	id of domain
 	 *	@param array $opt			associative array of options
 	 *	@param array $errors		error messages
 	 *	@return bool				TRUE on permit, FALSE on forbid, -1 on failure
 	 */ 
-	function check_admin_perms_to_domain($admin, $auth, $domain_id, $opt, &$errors){
-		if (isset($auth->auth['domains_perm']) and is_array($auth->auth['domains_perm'])){
-			if (in_array($domain_id, $auth->auth['domains_perm'])) return true;
+	function check_admin_perms_to_domain($admin, $domain_id, $opt, &$errors){
+		if (isset($admin->domains_perm) and is_array($admin->domains_perm)){
+			if (in_array($domain_id, $admin->domains_perm)) return true;
 		}
 		
 		return false;
--- serweb/data_layer/method.check_admin_perms_to_user.php
+++ serweb/data_layer/method.check_admin_perms_to_user.php
@@ -1,21 +1,44 @@
 <?
 /*
- * $Id: method.check_admin_perms_to_user.php,v 1.1 2004/08/25 10:45:58 kozlik Exp $
+ * $Id: method.check_admin_perms_to_user.php,v 1.2 2005/10/07 14:17:14 kozlik Exp $
  */
 
 class CData_Layer_check_admin_perms_to_user {
-	var $required_methods = array();
+
+	function _get_required_methods(){
+		global $config;
+
+		$out = array();
+		if ($config->multidomain) $out[] = 'get_domain';
+		if ($config->users_indexed_by=='uuid') $out[] = 'get_user_dom_from_uid';
+		return $out;
+	}
 	
 	/* check if $user domain is same as $admin domain */
 	function check_admin_perms_to_user($admin, $user, &$errors){
 		global $config;
+
+		/* if users are indexed by uuid, get the real domain from DB in order 
+		 * to domain can't be faked 
+		 */
 		if ($config->users_indexed_by=='uuid'){
 			if (false === $usr=$this->get_user_dom_from_uid($user->uuid, $errors)) return -1;
-			
-			return $admin->domain == $usr['domain'];
+			$user->domain = $usr['domain'];
 		}
-		else
+
+		if ($config->multidomain){
+			$opt['filter']['name'] = $user->domain;
+			if (false === $dom=$this->get_domain($opt, $errors)) return -1;
+			
+			$dom = reset($dom);					/* get first field of array */
+			if (false === $dom) return false;	/* if returned array is empty (domain not exists) */
+			
+			if (in_array($dom['id'], $admin->domains_perm)) return true;
+			return false;
+		}
+		else{
 			return $admin->domain == $user->domain;
+		}
 		
 	}
 	
