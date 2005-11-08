f8f92ffea69af2f533184ed59f01bd11c735caef
kozlik <kozlik>
2005-11-08 15:29:20 +0000

    added option to allow obtain users deleted before given timestamp

--- serweb/data_layer/method.get_sql_user_flags.php
+++ serweb/data_layer/method.get_sql_user_flags.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Id: method.get_sql_user_flags.php,v 1.1 2005/11/01 17:58:39 kozlik Exp $
+ * $Id: method.get_sql_user_flags.php,v 1.2 2005/11/08 15:29:20 kozlik Exp $
  */
 
 class CData_Layer_get_sql_user_flags {
@@ -21,6 +21,8 @@ class CData_Layer_get_sql_user_flags {
 		$cup = &$config->data_sql->usr_pref;
 		$cs  = &$config->data_sql->subscriber;
 
+	    $opt_deleted_before = (isset($opt['deleted_before'])) ? $opt['deleted_before'] : false;
+
 		$o = array();
 
 		$o['disabled']['cols'] = ", upd.".$cup->att_value." as user_disabled ";
@@ -30,7 +32,6 @@ class CData_Layer_get_sql_user_flags {
 					"(s.".$cs->uuid."=upd.".$cup->uuid." and upd.".$cup->att_name."='disabled') ":
 					"(s.".$cs->username."=upd.".$cup->username." and s.".$cs->domain."=upd.".$cup->domain." and upd.".$cup->att_name."='disabled') ");
 		$o['disabled']['where'] = " not (COALESCE(upd.".$cup->att_value.", 0) > 0) and ";
-					
 
 		$o['deleted']['cols'] = ", upx.".$cup->att_value." as user_deleted ";
 		$o['deleted']['from'] = 
@@ -38,9 +39,11 @@ class CData_Layer_get_sql_user_flags {
 				(($config->users_indexed_by=='uuid')?
 					"(s.".$cs->uuid."=upx.".$cup->uuid." and upx.".$cup->att_name."='deleted') ":
 					"(s.".$cs->username."=upx.".$cup->username." and s.".$cs->domain."=upx.".$cup->domain." and upx.".$cup->att_name."='deleted') ");
-		$o['deleted']['where'] = " not (COALESCE(upx.".$cup->att_value.", 0) > 0) and ";
-					
-					
+		$o['deleted']['where'] = 
+			$opt_deleted_before ? 
+				" upx.".$cup->att_value." < ".$opt_deleted_before." and upx.".$cup->att_value." > 0 and " : 
+				" not (COALESCE(upx.".$cup->att_value.", 0) > 0) and ";
+							
 		return $o;					
 	}
 	
