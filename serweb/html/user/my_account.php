5615bbcb633ea6542f4981b741681e022d02794f
kozlik <kozlik>
2005-12-14 16:27:11 +0000

    - removed apu_personal_data - removed apu_user_preferences - added apu_password_update - added apu_lang_select - added apu_attributes

--- serweb/html/user/my_account.php
+++ serweb/html/user/my_account.php
@@ -1,17 +1,17 @@
 <?php
 /*
- * $Id: my_account.php,v 1.11 2005/11/30 09:58:16 kozlik Exp $
+ * $Id: my_account.php,v 1.12 2005/12/14 16:27:11 kozlik Exp $
  */ 
 
-$_data_layer_required_methods=array('get_user_real_name');
+$_data_layer_required_methods=array();
 
 $_phplib_page_open = array("sess" => "phplib_Session",
 						   "auth" => "phplib_Auth",
 						   "perm" => "phplib_Perm");
 
-$_required_modules = array('user_preferences', 'usrloc', 'acl');
+$_required_modules = array('auth', 'attributes', 'usrloc', 'acl');
 
-$_required_apu = array('apu_personal_data', 'apu_user_preferences', 'apu_aliases', 'apu_acl', 'apu_usrloc'); 
+$_required_apu = array('apu_password_update', 'apu_attributes', 'apu_aliases', 'apu_acl', 'apu_usrloc', 'apu_lang_select'); 
 
 require "prepend.php";
 
@@ -21,15 +21,19 @@ if ($controler->come_from_admin_interface){
 	$page_attributes['selected_tab']="users.php";
 }
 else{
-	$page_attributes['user_name'] = $data->get_user_real_name($serweb_auth, $controler->errors);
+	$page_attributes['user_name'] = get_user_real_name($serweb_auth);
 }
 
-
-$pd			= new apu_personal_data();
-$usr_pref	= new apu_user_preferences();
+$pu			= new apu_password_update();
+$usr_pref	= new apu_attributes();
 $aliases	= new apu_aliases();
 $acl		= new apu_acl();
-$ul			= new apu_usrloc();
+$ls		    = new apu_lang_select();
+
+
+if ($config->allow_change_usrloc){
+	$ul			= new apu_usrloc();
+}
 
 
 /*
@@ -53,10 +57,12 @@ $att_desc['sw_user_status_visible'] = $lang_str['ff_status_visibility'];
 $att_desc['send_daily_missed_calls'] = $lang_str['ff_send_daily_missed_calls'];
 $usr_pref->set_opt('att_description', $att_desc);
 
+$usr_pref->set_opt('attrs_kind', 'user');
 
 
-$pd->set_opt('change_pass', $config->allow_change_password);
-$pd->set_opt('change_email', $config->allow_change_email);
+$pu->set_opt('change_pass', $config->allow_change_password);
+
+$ls->set_opt('save_to_user_attr', true);
 
 
 
@@ -64,6 +70,7 @@ $pd->set_opt('change_email', $config->allow_change_email);
 $cfg=new stdclass();
 $cfg->enable_dial_voicemail            = $config->enable_dial_voicemail;
 $cfg->enable_test_firewall             = $config->enable_test_firewall;
+$cfg->allow_change_usrloc              = $config->allow_change_usrloc;
 
 
 $smarty->assign_by_ref("config", $cfg);
@@ -76,21 +83,26 @@ $controler->add_reqired_javascript("functions.js");
 
 
 
-$controler->add_apu($pd);
+$controler->add_apu($pu);
 $controler->add_apu($aliases);
 $controler->add_apu($acl);
-$controler->add_apu($ul);
 $controler->add_apu($usr_pref);
+$controler->add_apu($ls);
 
-
-$controler->assign_form_name("pd", $pd);
+$controler->assign_form_name("pd", $pu);
 $controler->assign_form_name("pd", $usr_pref);
-$controler->assign_form_name("ul", $ul);
+$controler->assign_form_name("pd", $ls);
 
-$controler->set_submit_for_form("ul", 
-	array('type' => 'image',
-		  'text' => $lang_str['b_add'],
-		  'src'  => get_path_to_buttons("btn_add.gif", $sess_lang)));
+if ($config->allow_change_usrloc){
+	$controler->add_apu($ul);
+	$controler->assign_form_name("ul", $ul);
+
+	$controler->set_submit_for_form("ul", 
+		array('type' => 'image',
+			  'text' => $lang_str['b_add'],
+			  'src'  => get_path_to_buttons("btn_add.gif", $sess_lang)));
+}
+
 
 
 $controler->set_template_name('u_my_account.tpl');
