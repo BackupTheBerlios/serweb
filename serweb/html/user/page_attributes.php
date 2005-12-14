2a2228bac3e321e0c46e597d40c3f7a7d3e2b7bc
kozlik <kozlik>
2005-12-14 16:21:50 +0000

    created function for get user real name from attributes

--- serweb/html/user/page_attributes.php
+++ serweb/html/user/page_attributes.php
@@ -1,7 +1,24 @@
 <?
 /*
- * $Id: page_attributes.php,v 1.1 2004/08/25 10:19:48 kozlik Exp $
+ * $Id: page_attributes.php,v 1.2 2005/12/14 16:21:50 kozlik Exp $
  */ 
+
+function get_user_real_name($user){
+	global $config;
+
+	$at = &$config->attr_names;
+
+	$attrs = &User_Attrs::singleton($user->uuid);
+	if (false === $fname = $attrs->get_attribute($at['fname'])) return false;
+	if (false === $lname = $attrs->get_attribute($at['lname'])) return false;
+
+	return array(
+		'fname'=>$fname,
+		'lname'=>$lname,
+		'uname'=>$user->uname,
+		'realm'=>$user->domain);
+}
+
 
 $page_attributes=array(
 	'title' => $config->display_page_heading ? $config->domain." ".$lang_str['user_management'] : null,
