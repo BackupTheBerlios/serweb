eeb491fa77cc77481d3492e7b004390a82bf4140
kozlik <kozlik>
2005-12-22 12:53:36 +0000

    preferences replaced by attributes

--- serweb/html/admin/domain_attributes.php
+++ serweb/html/admin/domain_attributes.php
@@ -1,16 +1,16 @@
 <?php
 /*
- * $Id: domain_preferences.php,v 1.1 2005/10/19 11:16:11 kozlik Exp $
+ * $Id: domain_attributes.php,v 1.1 2005/12/22 12:53:36 kozlik Exp $
  */ 
 
 $_data_layer_required_methods=array();
 $_phplib_page_open = array("sess" => "phplib_Session",
-						   "auth" => "phplib_Pre_Auth",
+						   "auth" => "phplib_Auth",
 						   "perm" => "phplib_Perm");
 
-$_required_modules = array('domain_preferences');
+$_required_modules = array('attributes');
 
-$_required_apu = array('apu_domain_preferences'); 
+$_required_apu = array('apu_attributes'); 
 
 
 require "prepend.php";
@@ -19,11 +19,12 @@ $perm->check("admin");
 
 $page_attributes['selected_tab']="list_of_domains.php";
 
-$dp	= new apu_domain_preferences();
+$dp	= new apu_attributes();
 $dp->set_opt('redirect_on_update', 'list_of_domains.php');
+$dp->set_opt('attrs_kind', 'domain');
 
 $controler->add_apu($dp);
-$controler->set_template_name('a_domain_preferences.tpl');
+$controler->set_template_name('a_domain_attributes.tpl');
 $controler->start();
 
 ?>
