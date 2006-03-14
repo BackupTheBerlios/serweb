8a0f2bba09547f5633f48515f53aaf3a3e24b01a
kozlik <kozlik>
2006-03-14 16:14:46 +0000

    removed obsoleted files

--- serweb/data_layer/method.get_missed_calls_of_yesterday.php
+++ serweb/data_layer/method.get_missed_calls_of_yesterday.php
@@ -1,39 +1,28 @@
 <?
 /*
- * $Id: method.get_missed_calls_of_yesterday.php,v 1.1 2004/08/25 10:45:58 kozlik Exp $
+ * $Id: method.get_missed_calls_of_yesterday.php,v 1.2 2006/03/14 16:14:46 kozlik Exp $
  */
 
 class CData_Layer_get_missed_calls_of_yesterday {
 	var $required_methods = array();
 	
-	function get_missed_calls_of_yesterday($user, &$errors){
+	function get_missed_calls_of_yesterday($uid, $opt){
 		global $config;
 
-		if (!$this->connect_to_db($errors)) return false;
+		$errors = array();
+		if (!$this->connect_to_db($errors)) {
+			ErrorHandler::add_error($errors);
+			return false;
+		}
 	
-		if ($config->users_indexed_by=='uuid'){
-			$q="SELECT t1.from_uri, t1.sip_from, t1.time, t1.sip_status  ".
-				"FROM ".$config->data_sql->table_missed_calls." t1 ".
-				"WHERE t1.callee_UUID='".$user->uuid."' and ".
-						"date_format(t1.time, '%Y-%m-%d')=date_format(DATE_SUB(CURDATE(), INTERVAL 1 DAY), '%Y-%m-%d') ".
-				"ORDER BY time DESC ";
-		}
-		else{
-			$q="(SELECT t1.from_uri, t1.sip_from, t1.time, t1.sip_status  ".
-					"FROM ".$config->data_sql->table_missed_calls." t1 ".
-					"WHERE t1.username='".$user->uname."' and t1.domain='".$user->domain."' and ".
-						"date_format(t1.time, '%Y-%m-%d')=date_format(DATE_SUB(CURDATE(), INTERVAL 1 DAY), '%Y-%m-%d') ) ".
-				"UNION ".
-				"(SELECT t1.from_uri, t1.sip_from, t1.time, t1.sip_status ".
-					"FROM ".$config->data_sql->table_missed_calls." t1, ".$config->data_sql->table_aliases." t2 ".
-					"WHERE 'sip:".$user->uname."@".$user->domain."'".
-						"=t2.contact AND t2.username=t1.username AND t2.domain=t1.domain and ".
-						"date_format(t1.time, '%Y-%m-%d')=date_format(DATE_SUB(CURDATE(), INTERVAL 1 DAY), '%Y-%m-%d') ) ".
-				"ORDER BY time DESC ";
-		}
+		$q="SELECT from_uri, sip_from, request_timestamp, sip_status  ".
+			"FROM ".$config->data_sql->table_missed_calls." ".
+			"WHERE to_uid='".$uid."' and ".
+					"date_format(request_timestamp, '%Y-%m-%d')=date_format(DATE_SUB(CURDATE(), INTERVAL 1 DAY), '%Y-%m-%d') ".
+			"ORDER BY request_timestamp DESC ";
 
 		$res=$this->db->query($q);
-		if (DB::isError($res)) {log_errors($res, $errors); return false;}
+		if (DB::isError($res)) { ErrorHandler::log_errors($res); return false; }
 
 		$out=array();
 		while ($row=$res->fetchRow(DB_FETCHMODE_OBJECT)) $out[]=$row;
